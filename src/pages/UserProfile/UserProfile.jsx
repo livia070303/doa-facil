a38fbb68151743dfa6f6b4853738c8ae1f7101c8
Milena Aphiner <161851079+Milena-Aphiner@users.noreply.github.com>
2024-10-08 08:46:01 -0300

    adjust UserProfile page responsiveness

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -20,63 +20,63 @@ export const UserProfile = () => {
 
                 {/* Main Content */}
                 <main className="flex-1 bg-gray-100 p-10">
-                    {/* Seção de Informações do Usuário */}
-                    <   section className="flex-1 bg-white p-8 rounded shadow-md">
+                {/* Seção de Informações do Usuário */} 
+                <section className="flex-1 bg-white p-8 rounded shadow-md w-full max-w-3xl mx-auto">
+                    {/* Informações do Usuário */}
 
-                        {/* Informações do Usuário */}
-                        <div className="flex items-center mb-6">
-                            {/* Foto de Perfil */}
-                            <div className="w-24 h-24 rounded-full bg-gray-300 flex-shrink-0">
-                                <img src="/path/to/profile.jpg" alt="Foto de Perfil" className="w-full h-full rounded-full object-cover" />
-                            </div>
-                            {/* Nome */}
-                            <div className="ml-6 flex-1">
-                                <label className="block text-gray-700">Nome</label>
-                                <input type="text" className="mt-1 p-1 block w-full border border-gray-300 rounded-md" value="Nome do Usuário" readOnly />
-                            </div>
+                    <div className="flex flex-col sm:flex-row items-center mb-6">
+                        {/* Foto de Perfil */}
+                        <div className="w-24 h-24 rounded-full bg-gray-300 flex-shrink-0">
+                        <img src="/path/to/profile.jpg" alt="Foto de Perfil" className="w-full h-full rounded-full object-cover" />
                         </div>
-                        {/* Linha: Email e Telefone */}
-                        <div className="flex mb-6">
-                            <div className="w-1/2 mr-2">
-                                <label className="block text-gray-700">Email</label>
-                                <input type="email" className="mt-1 p-1 block w-full border border-gray-300 rounded-md" value="email@exemplo.com" readOnly />
-                            </div>
-                            <div className="w-1/2 ml-2">
-                                <label className="block text-gray-700">Telefone</label>
-                                <input type="tel" className="mt-1 p-1 block w-full border border-gray-300 rounded-md" value="(00) 00000-0000" readOnly />
-                            </div>
+                        {/* Nome */}
+                        <div className="mt-4 sm:mt-0 sm:ml-6 flex-1 w-full">
+                        <label className="block text-gray-700">Nome</label>
+                        <input type="text" className="mt-1 p-1 block w-full border border-gray-300 rounded-md" value="Nome do Usuário" readOnly />
                         </div>
-                        {/* Linha: Endereço e Cidade */}
-                        <div className="flex mb-6">
-                            <div className="w-1/2 mr-2">
-                                <label className="block text-gray-700">Endereço</label>
-                                <input type="text" className="mt-1 p-1 block w-full border border-gray-300 rounded-md" value="Rua Exemplo, 123" readOnly />
-                            </div>
-                            <div className="w-1/2 ml-2">
-                                <label className="block text-gray-700">Cidade</label>
-                                <input type="text" className="mt-1 p-1 block w-full border border-gray-300 rounded-md" value="Cidade Exemplo" readOnly />
-                            </div>
+                    </div>
+                    {/* Linha: Email e Telefone */}
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-6">
+                        <div>
+                        <label className="block text-gray-700">Email</label>
+                        <input type="email" className="mt-1 p-1 block w-full border border-gray-300 rounded-md" value="email@exemplo.com" readOnly />
                         </div>
-                        {/* Linha: CEP, UF e Botão */}
-                        <div className="flex items-end">
-                            <div className="w-1/3 mr-2">
-                                <label className="block text-gray-700">CEP</label>
-                                <input type="text" className="mt-1 p-1 block w-full border border-gray-300 rounded-md" value="00000-000" readOnly />
-                            </div>
-                            <div className="w-1/3 mx-2">
-                                <label className="block text-gray-700">UF</label>
-                                <select className="mt-1 p-1 block w-full border border-gray-300 rounded-md" disabled>
-                                    <option>SP</option>
-                                    {/* Outras opções */}
-                                </select>
-                            </div>
-                            <div className="w-1/3 ml-2">
-                                <button className="w-full bg-azul-claro text-white py-2 px-4 rounded-md hover:bg-azul-médio">
-                                    Editar Minhas Informações
-                                </button>
-                            </div>
+                        <div>
+                        <label className="block text-gray-700">Telefone</label>
+                        <input type="tel" className="mt-1 p-1 block w-full border border-gray-300 rounded-md" value="(00) 00000-0000" readOnly />
                         </div>
-                    </section>
+                    </div>
+                    {/* Linha: Endereço e Cidade */}
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-6">
+                        <div>
+                        <label className="block text-gray-700">Endereço</label>
+                        <input type="text" className="mt-1 p-1 block w-full border border-gray-300 rounded-md" value="Rua Exemplo, 123" readOnly />
+                        </div>
+                        <div>
+                        <label className="block text-gray-700">Cidade</label>
+                        <input type="text" className="mt-1 p-1 block w-full border border-gray-300 rounded-md" value="Cidade Exemplo" readOnly />
+                        </div>
+                    </div>
+                    {/* Linha: CEP, UF e Botão */}
+                    <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 items-end">
+                        <div>
+                        <label className="block text-gray-700">CEP</label>
+                        <input type="text" className="mt-1 p-1 block w-full border border-gray-300 rounded-md" value="00000-000" readOnly />
+                        </div>
+                        <div>
+                        <label className="block text-gray-700">UF</label>
+                        <select className="mt-1 p-1 block w-full border border-gray-300 rounded-md" disabled>
+                            <option>SP</option>
+                            {/* Outras opções */}
+                        </select>
+                        </div>
+                        <div>
+                        <button className="w-full bg-azul-claro text-white py-2 px-4 rounded-md hover:bg-azul-médio">
+                            Editar Minhas Informações
+                        </button>
+                        </div>
+                    </div>
+                </section>
 
                     {/* Minhas Doações */}
                     <section className="mt-8">
