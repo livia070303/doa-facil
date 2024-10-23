87bef8ded91c1c0dd697bc026ff3a3d76f6f89c5
P9DRIN <pedrohenriquelimasilva2837@hotmail.com>
2024-10-23 20:39:27 -0300

    fix: fix cors error

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    // baseURL: 'https://doa-facil-api-tkba.vercel.app',
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://doa-facil-api-tkba.vercel.app',
+    // baseURL: 'http://localhost:8000',
     withCredentials: true
 })  
