0090412171e5c7cb508de9b9b6c92ac180910e8c
Milena Aphiner <161851079+Milena-Aphiner@users.noreply.github.com>
2024-10-23 13:28:31 -0300

    modified layout CreateNeededProductPage

--- src/pages/ProductSelectionPage/ProductSelectionPage.jsx
+++ src/pages/ProductSelectionPage/ProductSelectionPage.jsx
@@ -25,7 +25,7 @@ const ProductSelectionPage = () => {
   return (
     <HeaderAndFooter>
       <HeaderAndFooterContainer className="flex p-4 sm:p-8 md:p-12 lg:p-24 flex-col gap-4">
-        <div className="relative bg-white shadow min-h-screen">
+        <div className="relative bg-white min-h-screen">
           <div className="container mx-auto flex flex-col lg:flex-row py-6 md:py-10 space-y-6 lg:space-y-0 lg:space-x-6">
             <Sidebar className="w-full lg:w-1/4 hidden lg:block" initialCategory={initialCategory} />
 
