78e87efcf0701b35df2718d437a8d7c4bf7245aa
Dayelle <147112614+DayPessotti@users.noreply.github.com>
2024-11-05 00:12:47 -0300

    modified:   src/pages/HomePage/components/MostSearchedItems.jsx modified:   src/pages/HomePage/components/ProductList.jsx

--- src/pages/HomePage/components/MostSearchedItems.jsx
+++ src/pages/HomePage/components/MostSearchedItems.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 export function MostSearchedItems({ imgSrc, title }) {
   return (
-    <div className="flex flex-col items-center p-6 bg-white border border-gray-200 rounded-lg shadow-lg w-full max-w-[280px]"> {/* Aumenta max-w */}
+    <div className="flex flex-col items-center p-4 bg-white border border-gray-200 rounded-lg shadow-lg w-full max-w-[220px]">
       <img
         src={imgSrc}
         alt={title}
-        className="w-full h-48 object-contain rounded-t-lg" // Aumenta a altura da imagem
-        style={{ maxHeight: '200px', minHeight: '150px' }} // Define uma altura padrão
+        className="w-full h-40 object-contain rounded-t-lg"
+        style={{ maxHeight: '160px', minHeight: '120px' }}
       />
       <div className="mt-4 text-center">
-        <h3 className="text-lg font-medium">{title}</h3>
+        <h3 className="text-md font-medium">{title}</h3>
       </div>
     </div>
   );
--- src/pages/HomePage/components/ProductList.jsx
+++ src/pages/HomePage/components/ProductList.jsx
@@ -21,7 +21,7 @@ export function ProductList() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6"> {/* Layout ajustado para responsividade */}
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {products.map((product) => (
         <Link to={`/product/${product._id}`} key={product._id}>
           <MostSearchedItems
