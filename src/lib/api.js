1031337a6cbc50db59b008630c63365d97c39101
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-11-05 22:06:48 -0300

    fix: fix url

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const isDev = import.meta.env.MODE === 'development'
 
 export const api = axios.create({
-    // baseURL: import.meta.env.VITE_API_URL,
-    baseURL: 'http://localhost:8000',
+    baseURL: import.meta.env.VITE_API_URL,
+    // baseURL: 'http://localhost:8000',
     withCredentials: true
 })  
